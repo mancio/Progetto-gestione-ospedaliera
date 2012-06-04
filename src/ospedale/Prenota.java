26af16e19f4eb998be0b9e31fe30bdb46593d37a
alex <alex@alex-laptop>
2012-06-04 20:18:07 +0200

    implementata classe Prenota (visualizza solo le visite disponibili senza fare la prenotazione)

--- src/ospedale/Prenota.java
+++ src/ospedale/Prenota.java
@@ -4,19 +4,64 @@
  */
 package ospedale;
 
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+
 /**
  *
  * @author mancio
  */
 public class Prenota extends javax.swing.JFrame {
-
+private String codice,pass,paziente,email,telef,resid,reparto;
+    private int is_admin;
+    private Database db=new Database("ospedale","root","lilli");;
+    private Vector risultati=null;
+    private boolean connesso;
     /**
      * Creates new form Prenota
      */
     public Prenota() {
-        initComponents();
+        //initComponents();
     }
-
+    public Prenota(String Cod_Fisc,String passw,String nome,String mail,String tel,String res,int adm,String rep){
+        initComponents();
+        codice=Cod_Fisc;
+        pass=passw;
+        paziente=nome;;
+        email=mail;
+        telef=tel;
+        resid=res;
+        is_admin=adm;
+        jLabel3.setText(paziente);
+        reparto=rep;
+        jLabel4.setText(reparto);
+        
+        connesso=db.connetti();
+        String SQL="select data,ora from visite where reparto='"+reparto+"' and priorita='0' order by data,ora asc;";
+        risultati=db.eseguiQuery(SQL);
+       
+        int i=0;
+        while ( i<risultati.size() ) {
+            System.out.println("Record numero " + (i+1));
+            String [] record = (String[]) risultati.elementAt(i);
+            for (int j=0; j<record.length; j++){
+                System.out.println( record[j] );
+            }
+            i++;
+        }   
+        
+        
+        /* int lunghezza=risultati.size();
+        String[] vettore=new String[lunghezza];
+        for(int i=0;i<lunghezza;i++){
+            vettore[i]=(String) risultati.get(i);
+            System.out.println(vettore[i]);
+        }*/
+        
+        
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -34,6 +79,9 @@ public class Prenota extends javax.swing.JFrame {
         jButtonConferma = new javax.swing.JButton();
         jButtonEsci = new javax.swing.JButton();
         jButtonIndietro = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -85,26 +133,36 @@ public class Prenota extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("Benvenuto:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButtonEsci)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButtonIndietro)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButtonConferma))
-                    .addComponent(label1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jRadioButtonPriorità)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel4))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButtonEsci)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jButtonIndietro)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButtonConferma))
+                            .addComponent(label1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jRadioButtonPriorità)
+                                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 453, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -119,12 +177,21 @@ public class Prenota extends javax.swing.JFrame {
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 183, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
-                .addComponent(jRadioButtonPriorità)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonConferma)
-                    .addComponent(jButtonEsci)
-                    .addComponent(jButtonIndietro))
+                    .addComponent(jRadioButtonPriorità)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButtonConferma)
+                            .addComponent(jButtonEsci)
+                            .addComponent(jButtonIndietro)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)))
                 .addContainerGap())
         );
 
@@ -133,7 +200,7 @@ public class Prenota extends javax.swing.JFrame {
 
     private void jButtonIndietroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonIndietroActionPerformed
         // TODO add your handling code here:
-        Paziente p=new Paziente();
+        Paziente p=new Paziente(codice,pass,paziente,email,telef,resid,is_admin);
         p.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButtonIndietroActionPerformed
@@ -145,6 +212,7 @@ public class Prenota extends javax.swing.JFrame {
         this.setVisible(false);
     }//GEN-LAST:event_jButtonEsciActionPerformed
 
+   
     /**
      * @param args the command line arguments
      */
@@ -182,7 +250,7 @@ public class Prenota extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
 
             public void run() {
-                new Prenota().setVisible(true);
+               // new Prenota().setVisible(true);
             }
         });
     }
@@ -191,6 +259,9 @@ public class Prenota extends javax.swing.JFrame {
     private javax.swing.JButton jButtonEsci;
     private javax.swing.JButton jButtonIndietro;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JRadioButton jRadioButtonPriorità;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
